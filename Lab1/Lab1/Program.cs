7e5054e13cb37cc8a1b066a1309bfd40f24f6c3d
Волков Илья <ellias_vo312@mail.ru>
2022-03-15 08:37:03 +0500

    Залил первую лабу

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text;
+
+namespace Lab1;
+
+public class Main1
+{
+    private static void Main()
+    {
+        // Пример работы RSA шифровальщика/дешифровальщика:
+
+        var rsa = new RSA();
+        rsa.Encrypt();
+        
+        // Результат шифрования в файле "output.txt"
+        // Дешифруем!
+        
+        rsa.Decrypt();
+        
+        // Результат дешифрования в "result.txt"
+    }
+}
